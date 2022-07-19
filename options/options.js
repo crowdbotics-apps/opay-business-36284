b46546580c756759c1a6aa3fa4034344f323068e
Crowdbotics <team@crowdbotics.com>
2022-07-19 13:23:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "opay_business_36284",
+  url: "https://opay_business_36284.botics.co",
+  api: "https://opay_business_36284.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
